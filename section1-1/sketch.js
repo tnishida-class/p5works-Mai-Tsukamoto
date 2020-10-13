dfe7d9c329665886aa2b5335990d894f0578bfb0
Mai-Tsukamoto <72617833+Mai-Tsukamoto@users.noreply.github.com>
2020-10-13 11:54:45 +0900

    Update 1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -9,7 +9,7 @@ function draw() {
   triangle(12,120,116,120,116,10);
   fill(94, 185, 84);
   triangle(0,108,106,108,106,0);
-  fill(255);
+  fill(150);
   textSize(32);
   textFont("serif");
   text("46", 68, 100);
