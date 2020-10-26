5399425df8cf699edb9bb5cecbd1070921bdaff4
Mai-Tsukamoto <72617833+Mai-Tsukamoto@users.noreply.github.com>
2020-10-26 23:25:32 +0900

    2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -3,14 +3,19 @@ function setup() {
   const blue = color(0, 51, 160);
   createCanvas(270, 180);
   // noStroke();
+  noStroke();
   background(255);
 
   let d = height / 9; // 縞1本の太さ
-
+  fill(blue);
   for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
-  }
+  // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
+  　
+  if(i % 2 ==　0){
+
+    rect(0, i * d, width, d);
+}
+}
 
   fill(blue);
   let size = d * 5;
@@ -18,4 +23,7 @@ function setup() {
 
   fill(255);
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  rect(0, 2*d, size, d);
+  rect(2*d, 0, d, size);
+
 }
