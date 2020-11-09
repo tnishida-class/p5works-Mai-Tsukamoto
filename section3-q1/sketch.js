0da81b2efc4d61c549bc8c02a14245734fb38d30
Mai-Tsukamoto <72617833+Mai-Tsukamoto@users.noreply.github.com>
2020-11-09 19:36:45 +0900

    Update 3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,17 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46", 20, 30);
 }
 
-function balloon(t){
+function balloon(t, x, y)
+{
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
   fill(0);
   rect(0, 0, w + p * 2, h + p * 2);
+  triangle(w + p * 2, (h + p * 2)/2-x/4, w + p * 2, (h + p * 2)/2+x/4, (w + p * 2)+y, (h + p * 2)/2)
   fill(255);
-  text(t, p, h + p);
+  text(t, p, h+p);
 }
